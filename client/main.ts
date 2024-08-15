f862cc6407085dcaa327ab5793a6004d21dddd52
zivtal <zivtal83@gmail.com>
2024-08-15 15:31:28 +0300

    scss support

--- client/main.ts
+++ client/main.ts
@@ -1,15 +1,15 @@
-import './style.css'
+import "./styles/main.scss";
 
-import { createSSRApp } from 'vue'
-import { buildRouter } from "./routes"
-import App from './App.vue'
+import { createSSRApp } from "vue";
+import { buildRouter } from "./routes";
+import App from "./App.vue";
 
 // SSR requires a fresh app instance per request, therefore we export a function
 // that creates a fresh app instance. If using Vuex, we'd also be creating a
 // fresh store here.
 export function createApp() {
-  const app = createSSRApp(App)
-  const router = buildRouter()
-  app.use(router)
-  return { app, router }
+  const app = createSSRApp(App);
+  const router = buildRouter();
+  app.use(router);
+  return { app, router };
 }
