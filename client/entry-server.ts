70d150d8998b0042a49a2cb2542bbe96c0855cd7
zivtal <zivtal83@gmail.com>
2024-08-17 09:49:54 +0300

    entry-server.ts insert #app div added

--- client/entry-server.ts
+++ client/entry-server.ts
@@ -4,6 +4,15 @@ import { createApp } from './main';
 export async function render(url: string) {
   const { app, router } = createApp();
 
+  const appDiv = (() => {
+    const div = document.createElement('div');
+    div.id = 'app';
+
+    return div;
+  })();
+
+  document.querySelector('body')?.appendChild(appDiv);
+
   await router.push(url);
   await router.isReady();
 
