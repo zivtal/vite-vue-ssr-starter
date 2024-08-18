d98987199edefb60a1c482a2569b28ffb67d5b91
zivtal <zivtal83@gmail.com>
2024-08-18 12:29:30 +0300

    createViteSSR major refactor

--- server/create-vite-ssr.ts
+++ server/create-vite-ssr.ts
@@ -1,4 +1,5 @@
 // create-vite-ssr.ts
+import type { SSRData } from './models/ssr-data';
 import type { Metadata } from './models/metadata';
 import { type ViteDevServer } from 'vite';
 import { type Express } from 'express';
@@ -7,29 +8,60 @@ import fs from 'node:fs';
 import config from './config';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { SSRData } from './models/ssr-data';
 
 let vite: ViteDevServer;
 
 export default async (
   app: Express,
-  isProd: boolean = !!config.IS_PROD_ENV
-): Promise<{
-  vite: ViteDevServer;
-  templateHtml: string;
-  ssrManifest?: Record<string, any>;
-  render: (url: string, data: Record<string, any>, metaData: Metadata) => Promise<string>;
-}> => {
+  isProduction: boolean = !!config.IS_PROD_ENV
+): Promise<{ vite: ViteDevServer; render: (url: string, data: Record<string, any>) => Promise<string> }> => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const resolve = (p: string) => path.resolve(__dirname, p);
 
-  const templateHtml = isProd ? fs.readFileSync(resolve('../dist/client/index.html'), 'utf-8') : fs.readFileSync(resolve('../index.html'), 'utf-8');
-  const ssrManifest = isProd ? JSON.parse(fs.readFileSync(resolve('../dist/client/.vite/ssr-manifest.json'), 'utf-8')) : undefined;
+  const indexHtml = loadIndexHtml(isProduction, resolve);
+  const ssrManifest = loadSSRManifest(isProduction, resolve);
 
-  if (!isProd) {
+  await serverInit(app, isProduction);
+
+  const render = async (url: string, data: SSRData = {}) => {
+    const ssrRender = await loadSSRRender(isProduction);
+
+    if (!isProduction) {
+      await vite.transformIndexHtml(url, indexHtml);
+    }
+
+    const rendered = await ssrRender(url, ssrManifest);
+    const appHtml = await ssrRender({ path: url, data: {} });
+
+    return buildHtml(indexHtml, appHtml, rendered.head, data);
+  };
+
+  return { vite, render };
+};
+
+const loadIndexHtml = (isProduction: boolean, resolve: (p: string) => string): string => {
+  const filePath = isProduction ? '../dist/client/index.html' : '../index.html';
+
+  return fs.readFileSync(resolve(filePath), 'utf-8');
+};
+
+const loadSSRManifest = (isProduction: boolean, resolve: (p: string) => string): Record<string, any> | undefined => {
+  if (!isProduction) {
+    return;
+  }
+
+  const manifestPath = '../dist/client/.vite/ssr-manifest.json';
+  return JSON.parse(fs.readFileSync(resolve(manifestPath), 'utf-8'));
+};
+
+const serverInit = async (app: Express, isProduction: boolean): Promise<void> => {
+  if (!isProduction) {
     const { createServer } = await import('vite');
-
-    vite = await createServer({ appType: 'custom', server: { middlewareMode: true }, base: config.BASE_URL });
+    vite = await createServer({
+      appType: 'custom',
+      server: { middlewareMode: true },
+      base: config.BASE_URL,
+    });
     app.use(vite.middlewares);
   } else {
     const compression = (await import('compression')).default;
@@ -38,87 +70,72 @@ export default async (
     app.use(compression());
     app.use(config.BASE_URL, sirv('./dist/client', { extensions: [] }));
   }
+};
 
-  const renderStyle = (cheerioApi: cheerio.CheerioAPI, style?: SSRData['style']): void => {
-    if (!style) {
-      return;
-    }
+const loadSSRRender = async (isProduction: boolean): Promise<any> => {
+  // @ts-ignore
+  return isProduction ? (await import('../dist/server/entry-server.js')).render : (await vite.ssrLoadModule('./client/entry-server.ts')).render;
+};
 
-    const cssStyle = Object.entries(style || {})
-      .map(([selector, properties]) => {
-        return `${selector} {\n${Object.entries(properties)
-          .map(([property, value]) => ` ${property}: ${value};`)
-          .join('\n')}\n}`;
-      })
-      .join('\n\n');
+const buildHtml = (templateHtml: string, appHtml: string, headContent: string, data: SSRData): string => {
+  const { metadata, direction = 'ltr', style, ...state } = data;
+  const cheerioApi = cheerio.load(templateHtml);
 
-    cheerioApi('head').append(`<style>${cssStyle}</style>`);
-  };
+  cheerioApi('html').attr('dir', direction as string);
+  cheerioApi('head').find('title').text(state.title);
+  cheerioApi('head').append(headContent);
+  cheerioApi('head').append(`<link rel="manifest" href="/manifest.json">`);
+  cheerioApi('#root').html(appHtml);
+  cheerioApi('body').append(`<script id="ssr">window.__SSR_DATA__=${JSON.stringify(state)};document.getElementById('ssr').remove();</script>`);
 
-  const renderMeta = (cheerioApi: cheerio.CheerioAPI, metadata?: Metadata): void => {
-    if (metadata?.image) {
-      cheerioApi('head').append(
-        `<meta name="image" content="${metadata.image}" />`,
-        `<meta property="og:image" content="${metadata.image}" />`,
-        `<meta property="twitter:image" content="${metadata.image}" />`
-      );
-    }
+  renderMeta(cheerioApi, metadata);
+  renderStyle(cheerioApi, style);
 
-    if (metadata?.title) {
-      cheerioApi('head').append(
-        `<meta property="og:title" content="${metadata.title}" />`,
-        `<meta property="og:site_name" content="${metadata.title}" />`,
-        `<meta property="twitter:title" content="${metadata.title}" />`
-      );
-    }
+  return cheerioApi.html();
+};
 
-    if (metadata?.description) {
-      cheerioApi('head').append(
-        `<meta name="description" content="${metadata.description}" />`,
-        `<meta property="og:description" content="${metadata.description}" />`,
-        `<meta property="twitter:description" content="${metadata.description}" />`
-      );
-    }
+const renderStyle = (cheerioApi: cheerio.CheerioAPI, style?: SSRData['style']): void => {
+  if (!style) {
+    return;
+  }
 
-    if (metadata?.keywords?.length) {
-      cheerioApi('head').append(`<meta name="keywords" content="${(metadata.keywords || []).join(',')}" />`);
-    }
-  };
+  const cssStyle = Object.entries(style)
+    .map(([selector, properties]) => {
+      return `${selector} {\n${Object.entries(properties)
+        .map(([property, value]) => ` ${property}: ${value};`)
+        .join('\n')}\n}`;
+    })
+    .join('\n\n');
 
-  const render = async (url: string, data: SSRData = {}) => {
-    return await (async () => {
-      const ssrRender = isProd
-        ? // @ts-ignore
-          (await import('../dist/server/entry-server.js')).render // workspace added for digitalocean, i fix your path for entry-server on dist folder
-        : (await vite.ssrLoadModule('./client/entry-server.ts')).render;
+  cheerioApi('head').append(`<style>${cssStyle}</style>`);
+};
 
-      if (!isProd) {
-        await vite.transformIndexHtml(url, templateHtml);
-      }
+const renderMeta = (cheerioApi: cheerio.CheerioAPI, metadata?: Metadata): void => {
+  if (metadata?.image) {
+    cheerioApi('head').append(
+      `<meta name="image" content="${metadata.image}" />`,
+      `<meta property="og:image" content="${metadata.image}" />`,
+      `<meta property="twitter:image" content="${metadata.image}" />`
+    );
+  }
 
-      const rendered = await ssrRender(url, ssrManifest);
-      const appHtml = await ssrRender({ path: url, data: {} });
+  if (metadata?.title) {
+    cheerioApi('head').append(
+      `<meta property="og:title" content="${metadata.title}" />`,
+      `<meta property="og:site_name" content="${metadata.title}" />`,
+      `<meta property="twitter:title" content="${metadata.title}" />`
+    );
+  }
 
-      const { metadata, direction = 'ltr', style, ...state } = data;
+  if (metadata?.description) {
+    cheerioApi('head').append(
+      `<meta name="description" content="${metadata.description}" />`,
+      `<meta property="og:description" content="${metadata.description}" />`,
+      `<meta property="twitter:description" content="${metadata.description}" />`
+    );
+  }
 
-      const cheerioApi = cheerio.load(templateHtml);
-      cheerioApi('html').attr('dir', direction as string);
-      cheerioApi('head').find('title').text(state.title);
-      cheerioApi('head').append(rendered.head);
-      cheerioApi('head').append(`<link rel="manifest" href="/manifest.json">`);
-      cheerioApi('#root').html(appHtml);
-      cheerioApi('body').append(`<script id="ssr">window.__SSR_DATA__=${JSON.stringify(state)};document.getElementById('ssr').remove();</script>`);
-      renderMeta(cheerioApi, metadata);
-      renderStyle(cheerioApi, style);
-
-      return cheerioApi.html();
-    })();
-  };
-
-  return {
-    vite,
-    templateHtml,
-    ssrManifest,
-    render,
-  };
+  if (metadata?.keywords?.length) {
+    cheerioApi('head').append(`<meta name="keywords" content="${metadata.keywords.join(',')}" />`);
+  }
 };
