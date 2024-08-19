1af641727e81c993fd17607a213a66bc317e3b65
zivtal <zivtal83@gmail.com>
2024-08-20 00:18:27 +0300

    webConstants:GET_WEBSITE_CONTENT removed

--- server/api/web/web.constants.ts
+++ server/api/web/web.constants.ts
@@ -3,8 +3,5 @@ export const GET_CONTENT = 'get-content';
 export const FAVICON = 'favicon';
 export const MANIFEST = 'manifest';
 
-// remote api
-export const GET_WEBSITE_CONTENT = 'get-website-content';
-
 // keys
 export const ID_KEY = 'templateId';
