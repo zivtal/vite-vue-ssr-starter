1a28c677b49fec6ecb54ee6ae9b1c24802d467c8
zivtal <zivtal83@gmail.com>
2024-08-19 19:10:13 +0300

    config env improved

--- server/config/index.ts
+++ server/config/index.ts
@@ -1,11 +1,22 @@
 import dotenv from 'dotenv';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-dotenv.config();
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const resolve = (p: string) => path.resolve(__dirname, p);
+
+try {
+  dotenv.config({ path: resolve(!IS_PRODUCTION ? '../../.env' : '../../.env.development') });
+} catch (e) {
+  console.error('dotenv:file', e);
+  dotenv.config();
+}
 
 export default {
-  IS_PROD_ENV: process.env.NODE_ENV === 'production',
-  PORT: process.env.PORT || 4000,
-  BASE_URL: process.env.BASE_URL || '/',
-  BASE_API: process.env.BASE_API || 'http://localhost:3030/web',
+  IS_PROD_ENV: IS_PRODUCTION,
+  PORT: process.env.PORT,
+  BASE_URL: process.env.BASE_URL,
+  BASE_API: process.env.BASE_API,
   SESSION_KEY: process.env.SESSION_KEY,
 };
